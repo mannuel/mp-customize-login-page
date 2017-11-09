dea8a6ee396917e329a3cb0a0a1158469b77fb89
Manuel Padilla <manuel@plugdigital.net>
2017-11-09 16:49:51 -0600

    changing the code organization

--- class.mp-customize-login-page.php
+++ class.mp-customize-login-page.php
@@ -33,6 +33,7 @@ class mpclp {
 		add_filter( 'login_headerurl', array( 'mpclp', 'mpclp_logo_url' ) );
 		add_filter( 'login_headertitle', array( 'mpclp', 'mpclp_logo_title' ) );
 		add_filter( 'plugin_action_links_'.MPCLP__PLUGIN_BASENAME, array( 'mpclp', 'mpclp_plugin_action_links' ));
+		add_action( 'admin_init', array( 'mpclp', 'register_settings' ) );
 	}
 
 	/**
@@ -161,71 +162,70 @@ class mpclp {
 	public static function mpclp_logo_title() {
 		return get_bloginfo( 'name' );
 	}
+
+	/**
+	 * 
+	 */
+	public static function register_settings() {
+		register_setting( 'mpclp_options', 'mpclp' );
+
+		add_settings_section( 'mpclp_opstions_section', 'Page options', array( 'mpclp', 'mpclp_opstions_fields' ), 'MP Customize Login Page' );
+		add_settings_section( 'mpclp_opstions_form_section', 'Form options', array( 'mpclp', 'mpclp_opstions_form_fields'), 'MP Customize Form Login Page' );
+
+		add_settings_field( 'mpclp-login-image', 'Logo image', array( 'mpclp', 'mpclp_opt_logo_image'), 'MP Customize Login Page', 'mpclp_opstions_section', '' );
+		add_settings_field( 'mpclp-login-image-link', 'Logo link', array( 'mpclp', 'mpclp_opt_login_image_link'), 'MP Customize Login Page', 'mpclp_opstions_section', '' );
+		add_settings_field( 'mpclp-login-image-height', 'Login image height', array( 'mpclp', 'mpclp_opt_login_image_height'), 'MP Customize Login Page', 'mpclp_opstions_section', '' );
+		add_settings_field( 'mpclp-login-form-background', 'Page background', array( 'mpclp', 'mpclp_opt_page_background'), 'MP Customize Login Page', 'mpclp_opstions_section', '' );
+
+		add_settings_field( 'mpclp-login-form-background', 'Form background', array( 'mpclp', 'mpclp_login_form_background'), 'MP Customize Form Login Page', 'mpclp_opstions_form_section', '' );
+		add_settings_field( 'mpclp-login-form-label', 'Form label color', array( 'mpclp', 'mpclp_login_form_label'), 'MP Customize Form Login Page', 'mpclp_opstions_form_section', '' );
+		add_settings_field( 'mpclp-login-message', 'Message', array( 'mpclp', 'mpclp_login_message'), 'MP Customize Form Login Page', 'mpclp_opstions_form_section', '' );
+	}
+
+	public static function mpclp_opstions_fields(){
+		echo "Test";
+	}
+
+	public static function mpclp_opstions_form_fields(){
+		echo "Set styles for your login form";
+	}
+
+	public static function mpclp_opt_logo_image( ){
+		$mpclp_login_image = esc_attr( get_option( 'mpclp_login_image' ) );
+		echo '<input type="hidden" name="mpclp-login-image" id="mpclp-login-image" value="'.$mpclp_login_image.'">';
+		echo '<input type="button" value="Set login image" id="upload-login-image-button" class="button">';
+	}
+
+	public static function mpclp_opt_login_image_link( ){
+		$mpclp_login_image_link = esc_attr( get_option( 'mpclp_login_image_link' ) );
+		echo '<input type="url" id="mpclp-login-image-link" name="mpclp-login-image-link" value="'.$mpclp_login_image_link.'" placeholder="'.home_url().'">';
+	}
+
+	public static function mpclp_opt_login_image_height( ){
+		$mpclp_login_image_height = esc_attr( get_option( 'mpclp_login_image_height' ) );
+		echo '<input type="text" id="mpclp-login-image-height" name="mpclp-login-image-height" value="'.$mpclp_login_image_height.'" placeholder="100px">';
+	}
+
+	public static function mpclp_opt_page_background( ){
+		$mpclp_login_background = esc_attr( get_option( 'mpclp_login_background' ) );
+		echo '<input type="text" id="mpclp-login-background" class="wpColorPicker" name="mpclp-login-background" value="'.$mpclp_login_background.'" />';
+	}
+
+	public static function mpclp_login_form_background( ){
+		$mpclp_login_form_background = esc_attr( get_option( 'mpclp_login_form_background' ) );
+		echo '<input type="text" id="mpclp-login-form-background" class="wpColorPicker" name="mpclp-login-form-background" value="'.$mpclp_login_form_background.'">';
+	}
+
+	public static function mpclp_login_form_label( ){
+		$mpclp_login_form_label = esc_attr( get_option( 'mpclp_login_form_label' ) );
+		echo '<input type="text" class="wpColorPicker" name="mpclp-login-form-label" value="'.$mpclp_login_form_label.'" id="mpclp-login-form-label">';
+	}
+
+	public static function mpclp_login_message( ){
+		$mpclp_login_message = html_entity_decode(get_option( 'mpclp_login_message' ));
+		$settings = array(
+			'editor_height' => 70 
+		);
+		wp_editor( $mpclp_login_message, 'mpclp-login-message', $settings );
+	}
 } // class mpclp end
-
-
-/**
- * 
- */
-function register_settings() {
-	register_setting( 'mpclp_options', 'mpclp' );
-	add_settings_section( 'mpclp_opstions_section', 'Page options', 'mpclp_opstions_fields', 'MP Customize Login Page' );
-	add_settings_section( 'mpclp_opstions_form_section', 'Form options', 'mpclp_opstions_form_fields', 'MP Customize Form Login Page' );
-
-	add_settings_field( 'mpclp-login-image', 'Logo image', 'mpclp_opt_logo_image', 'MP Customize Login Page', 'mpclp_opstions_section', '' );
-	add_settings_field( 'mpclp-login-image-link', 'Logo link', 'mpclp_opt_login_image_link', 'MP Customize Login Page', 'mpclp_opstions_section', '' );
-	add_settings_field( 'mpclp-login-image-height', 'Login image height', 'mpclp_opt_login_image_height', 'MP Customize Login Page', 'mpclp_opstions_section', '' );
-	add_settings_field( 'mpclp-login-form-background', 'Page background', 'mpclp_opt_page_background', 'MP Customize Login Page', 'mpclp_opstions_section', '' );
-
-	add_settings_field( 'mpclp-login-form-background', 'Form background', 'mpclp_login_form_background', 'MP Customize Form Login Page', 'mpclp_opstions_form_section', '' );
-	add_settings_field( 'mpclp-login-form-label', 'Form label color', 'mpclp_login_form_label', 'MP Customize Form Login Page', 'mpclp_opstions_form_section', '' );
-	add_settings_field( 'mpclp-login-message', 'Message', 'mpclp_login_message', 'MP Customize Form Login Page', 'mpclp_opstions_form_section', '' );
-}
-add_action( 'admin_init', 'register_settings' );
-
-function mpclp_opstions_fields(){
-	echo "";
-}
-
-function mpclp_opstions_form_fields(){
-	echo "Set styles for your login form";
-}
-
-function mpclp_opt_logo_image( ){
-	$mpclp_login_image = esc_attr( get_option( 'mpclp_login_image' ) );
-	echo '<input type="hidden" name="mpclp-login-image" id="mpclp-login-image" value="'.$mpclp_login_image.'">';
-	echo '<input type="button" value="Set login image" id="upload-login-image-button" class="button">';
-}
-
-function mpclp_opt_login_image_link( ){
-	$mpclp_login_image_link = esc_attr( get_option( 'mpclp_login_image_link' ) );
-	echo '<input type="url" id="mpclp-login-image-link" name="mpclp-login-image-link" value="'.$mpclp_login_image_link.'" placeholder="'.home_url().'">';
-}
-
-function mpclp_opt_login_image_height( ){
-	$mpclp_login_image_height = esc_attr( get_option( 'mpclp_login_image_height' ) );
-	echo '<input type="text" id="mpclp-login-image-height" name="mpclp-login-image-height" value="'.$mpclp_login_image_height.'" placeholder="100px">';
-}
-
-function mpclp_opt_page_background( ){
-	$mpclp_login_background = esc_attr( get_option( 'mpclp_login_background' ) );
-	echo '<input type="text" id="mpclp-login-background" class="wpColorPicker" name="mpclp-login-background" value="'.$mpclp_login_background.'" />';
-}
-
-function mpclp_login_form_background( ){
-	$mpclp_login_form_background = esc_attr( get_option( 'mpclp_login_form_background' ) );
-	echo '<input type="text" id="mpclp-login-form-background" class="wpColorPicker" name="mpclp-login-form-background" value="'.$mpclp_login_form_background.'">';
-}
-
-function mpclp_login_form_label( ){
-	$mpclp_login_form_label = esc_attr( get_option( 'mpclp_login_form_label' ) );
-	echo '<input type="text" class="wpColorPicker" name="mpclp-login-form-label" value="'.$mpclp_login_form_label.'" id="mpclp-login-form-label">';
-}
-
-function mpclp_login_message( ){
-	$mpclp_login_message = html_entity_decode(get_option( 'mpclp_login_message' ));
-	$settings = array(
-		'editor_height' => 70 
-	);
-	wp_editor( $mpclp_login_message, 'mpclp-login-message', $settings );
-}
